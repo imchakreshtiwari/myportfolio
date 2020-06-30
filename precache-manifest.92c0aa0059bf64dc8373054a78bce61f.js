e9b88864326f02fb8ec46f150459961a66d3d52d
chakresh <chakresh@acis.io>
2020-06-30 16:48:46 +0530

    Updates

---
+++ precache-manifest.92c0aa0059bf64dc8373054a78bce61f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaade25d906a45883091182e66641b2",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "706b64bd1e976546159b",
+    "url": "/myportfolio/static/css/main.9b0ccc5f.chunk.css"
+  },
+  {
+    "revision": "21277efb601478d59148",
+    "url": "/myportfolio/static/js/2.541cbd9e.chunk.js"
+  },
+  {
+    "revision": "1e3bf01ccf525cade81508e70537c92f",
+    "url": "/myportfolio/static/js/2.541cbd9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706b64bd1e976546159b",
+    "url": "/myportfolio/static/js/main.6d0591a3.chunk.js"
+  },
+  {
+    "revision": "6600aec0acac4ebef96a",
+    "url": "/myportfolio/static/js/runtime-main.4f0cea2f.js"
+  },
+  {
+    "revision": "0f8c09cd491ab8cb4053728b56f37013",
+    "url": "/myportfolio/static/media/profilepic.0f8c09cd.jpg"
+  }
+]);
